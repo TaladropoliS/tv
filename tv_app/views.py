364d53abbb0d8e197f5ed1169a26b2bb0170fa0b
TaladropoliS <fernando.taladriz@gmail.com>
2021-03-09 20:12:35 -0300

    logo

--- tv_app/views.py
+++ tv_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 # from .models import Usuario, Amigo
-from django.contrib import messages
-from django.db.models import Q, Max, Count, F
+# from django.contrib import messages
+# from django.db.models import Q, Max, Count, F
 
 def inicio(request):
 
