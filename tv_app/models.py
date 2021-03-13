3fd3eb73e5c87d2794eccbfbc64eb12f13c2d062
TaladropoliS <fernando.taladriz@gmail.com>
2021-03-13 06:56:30 -0300

    ok validadores de form backend

--- tv_app/models.py
+++ tv_app/models.py
@@ -1,20 +1,24 @@
 from __future__ import unicode_literals
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
 class TvManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
-        if len(postData['titulo']) < 1:
-            errors["titulo"] = "Debes ingresar el Título"
-        if len(postData['canal']) < 1:
-            errors["canal"] = "Debes ingresar el Canal"
-        if len(postData['fecha']) < 1:
-            errors["fecha"] = "Debes ingresar una Fecha"
+        if len(postData['titulo']) < 2:
+            errors["titulo"] = "Ingresar Título, mínimo 2 caracteres."
+        if len(postData['canal']) < 3:
+            errors["canal"] = "Ingresar Canal, mínimo 3 caracteres."
+        if len(postData['fecha']) < 8:
+            errors["fecha"] = "Ingresar Fecha, no superior a la de hoy."
         if len(postData['desc']) < 1:
-            errors["desc"] = "Debes ingresar una Descripción"
-        return errors
+            return errors
+        else:
+            if len(postData['desc']) < 10:
+                errors["desc"] = "Ingresar Descripción, mínimo 10 caracteres."
+            return errors
 
 class Programa(models.Model):
     titulo = models.CharField(max_length=255)
